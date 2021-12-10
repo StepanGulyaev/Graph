493a5a0e9d1e4f19e94d660e3a8eb5d65b3e4cba
StepanGulyaev <91908161+StepanGulyaev@users.noreply.github.com>
2021-12-10 14:45:46 +0300

    Update graph.h

--- graph.h
+++ graph.h
@@ -14,7 +14,9 @@ class Graph
         int flag = 0;
 
         int check_node_exist(int);
-        //int check_way_exist(size_t,size_t);
+        void delete_loop(int);
+        int check_way_exist(int,int);
+        void print_ways(std::vector<std::vector<int>>);
         void find_simple_connects(int,int,std::vector<int> &way,std::vector<std::vector<int>> &ways);
 
     public:
@@ -25,12 +27,11 @@ class Graph
 
         ~Graph();
 
-        void delete_loop(int);
+        
         void add_node(std::vector<int>);
         void fill_matrix();
         void print_matrix();
         void print_simple_connects();
-        int check_way_exist(int,int);
     }; 
 
 
